91f0f6dc877dc54f63906b486fd61a133b29edc0
Andri Ihsannudin <andri.ihsannudin@gmail.com>
2017-12-03 00:21:14 +0700

    Solve Day 1 Part 2 and Day 2 Part 2 - Renaming stuffs

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,11 +1,22 @@
 object Day1 {
-    fun reverseCaptcha(input: String) = reverseCaptcha(input.map { "$it".toLong() })
+    fun part1Solution(input: String) = captchaPart1(input.map { "$it".toLong() })
+    fun part2Solution(input: String) = captchaPart2(input.map { "$it".toLong() })
 
     data class Result(val sum: Long, val current: Long)
 
-    fun reverseCaptcha(inputs: List<Long>): Long = inputs.fold(Result(0, inputs.last())) { accum, input -> when(input) {
+    fun captchaPart1(inputs: List<Long>): Long = inputs.fold(Result(0, inputs.last())) { accum, input -> when(input) {
         accum.current -> Result(accum.sum + input, input)
         else -> accum.copy(current = input)
-    }
+        }
     }.sum
+
+    fun captchaPart2(inputs: List<Long>): Long {
+        val middle = inputs.size / 2
+        return inputs.take(inputs.size / 2).foldIndexed(0L) { index, accum, current ->
+            when(current) {
+                inputs[middle + index] -> accum + current + inputs[middle + index]
+                else ->accum
+            }
+        }
+    }
 }
--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,7 +1,12 @@
 object Day2 {
-    fun calculateDay2Checksum(input: String): Long = splitRows(input)
+    fun part1Solution(input: String): Long = splitRows(input)
             .map { splitColumns(it) }
             .map { minMaxDiff(it) }
+            .sum()
+
+    fun part2Solution(input: String): Long = splitRows(input)
+            .map { splitColumns(it) }
+            .map { wholeDivider(it) }
             .sum()
 
     fun minMaxDiff(input: List<Long>): Long {
@@ -9,6 +14,11 @@ object Day2 {
         val max = requireNotNull(input.max()) { "Cannot found max" }
         return Math.abs(max - min)
     }
+
+    fun wholeDivider(input: List<Long>): Long = input.mapIndexed { index, value ->
+        val result = input.drop(index + 1).find { it % value == 0L || value % it == 0L }
+        result?.let { Math.max(result, value) / Math.min(result, value) }
+    }.filterNotNull().first()
 
     fun splitRows(input: String): List<String> = input.split("\n").filter { it.isNotBlank() }
     fun splitColumns(input: String): List<Long> = input.split(Regex("[ \t]")).map { it.toLong() }
